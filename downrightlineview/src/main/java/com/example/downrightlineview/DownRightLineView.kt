07e54be0cee4f8d8206fda982ff322d454a8d151
Anwesh43 <anweshthecool0@gmail.com>
2021-07-10 19:44:34 +0530

    adding renderer in view

--- downrightlineview/src/main/java/com/example/downrightlineview/DownRightLineView.kt
+++ downrightlineview/src/main/java/com/example/downrightlineview/DownRightLineView.kt
@@ -57,14 +57,16 @@ fun Canvas.drawDRLNode(i : Int, scale : Float, paint : Paint) {
 
 class DownRightLineView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
